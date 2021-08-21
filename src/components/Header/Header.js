b380d6e22fae12c5bc9406e4f03e8346df8e6afd
gazmagamaev <magamedali93@mail.ru>
2021-08-21 22:29:31 +0300

    react-samuray/1

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css'
+
+function Header(props) {
+    return (
+        <div className={s.item}>шапка сайта</div>
+    );
+}
+
+export default Header;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Navbar(props) {
+    return (
+        <div>навбар</div>
+    );
+}
+
+export default Navbar;
